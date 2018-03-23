886e595736ad2947d5a2e7246527d12b386348af
Reka Kulinyi <reekaa85@gmail.com>
2018-03-23 17:49:41 +0100

    finished exercise

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,33 @@
+function newQuote() {
+  $.ajax({
+    url:
+      "https://api.forismatic.com/api/1.0/?method=getQuote&key=XXXXX&format=jsonp&jsonp=?&lang=en",
+    dataType: "jsonp",
+    success: function(json) {
+      if (json.quoteAuthor.length === 0) {
+        author = "Unknown";
+      } else {
+        author = json.quoteAuthor;
+      }
+
+      $("#field").html(`<p>\"${json.quoteText}\"</p><p>${author}</p>`)
+      tweetText = `\"${json.quoteText}\" by ${json.quoteAuthor}`
+
+      if (tweetText.length > 280){
+        tweetText = tweetText.slice(0, 280)
+      }
+
+      tweetUrl = `https://twitter.com/intent/tweet?text=+${tweetText}`;
+      $("#twitter-button").attr("href", tweetUrl);
+    },
+    error: function(xhr, status, error) {
+      $("#field").html("<p>Sorry, there was error. Try again!</p>");
+    }
+  });
+}
 
 $(document).ready(function(){
   $("#quote-button").on("click", function(){
-    const quotes = { "Steve": "Very good quote by Steven",
-                     "John": "quote from John",
-                     "Mary": "quote from Mary" };
-    const keys = Object.keys(quotes);
-    const author = keys[ Math.floor(Math.random() * keys.length)];
-    const quote = quotes[author];
-    $("#field").html(`<p>${quote} by <em>${author}</p>`)
+    newQuote();
   });
 });
